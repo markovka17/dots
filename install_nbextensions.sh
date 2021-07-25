17231fbf5af8ac0898f4bf39b7cd36d484b58048
Alexander Markovich <oMa654@yandex.ru>
2021-07-25 14:12:20 +0300

    Add files via upload

---
+++ install_nbextensions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pip install notebook
+pip install "nbconvert<6"
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+
+jupyter nbextension enable code_prettify/autopep8
+jupyter nbextension enable codefolding/edit
+
+mkdir --parent ~/.jupyter/custom/
+echo ".container { width:95% !important; }" > ~/.jupyter/custom/custom.css
+
